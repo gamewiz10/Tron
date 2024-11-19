913189f06657ca7cf6f33b8cf077d682e01566a2
gamewiz10 <joseph.bell.20@cnu.edu>
2024-11-19 12:17:05 -0500

    gameThread push

--- app/src/main/java/com/example/tron/GameActivity.java
+++ app/src/main/java/com/example/tron/GameActivity.java
@@ -1,4 +1,37 @@
 package com.example.tron;
 
-public class GameActivity {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import androidx.annotation.NonNull;
+
+
+public class GameActivity extends Activity implements SurfaceHolder.Callback {
+
+    private SurfaceView gameSurfaceView;
+    private GameThread gameThread;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_activity);
+
+        gameSurfaceView = findViewById(R.id.gameSurfaceView)
+    }
+
+    @Override
+    public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+    }
 }
